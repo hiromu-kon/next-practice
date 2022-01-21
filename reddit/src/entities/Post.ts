278af780e556e1faffe20e68bfab8eefcec907e8
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-01-21 22:43:11 +0900

    add

--- reddit/src/entities/Post.ts
+++ reddit/src/entities/Post.ts
@@ -1,6 +1,8 @@
 import { IsEmail, Length } from "class-validator";
-import { Entity as TOEntity } from "typeorm";
+import { BeforeInsert, Column, Entity as TOEntity, Index, JoinColumn, ManyToOne } from "typeorm";
+import { makeId, slugify } from "../util/helpers";
 import Entity from "./Entity";
+import User from "./User";
 
 @TOEntity('posts')
 export default class Post extends Entity {
@@ -10,5 +12,30 @@ export default class Post extends Entity {
         Object.assign(this, Post)
     }
 
+    @Index()
+    @Column()
+    identifier: string // 7 Character Id
 
+    @Column()
+    title: string
+
+    @Index()
+    @Column()
+    slug: string
+
+    @Column({ nullable: true, type: 'text' })
+    body: string
+
+    @Column()
+    subName: string
+
+    @ManyToOne(() => User, user => user.posts)
+    @JoinColumn({ name: 'username', referencedColumnName: 'username' })
+    user: User;
+
+    @BeforeInsert()
+    makeIdAndSlug() {
+        this.identifier = makeId(7)
+        this.slug = slugify(this.title)
+    }
 }
