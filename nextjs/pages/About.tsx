20e6d240330a10563dc09e8209d012b4407bbdd2
hiromu-kon <hiro.0405soccer@icloud.com>
2022-01-22 22:51:32 +0900

    fix

---
+++ nextjs/pages/About.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link href="/">
+        <a>Homeページへ</a>
+      </Link>
+    </div>
+  )
+}
+
+export default About
