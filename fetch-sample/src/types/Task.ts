bd5a037d76fb3aeca5fe4a6bbdc392bb23df20db
hiromu-kon <hiro.0405soccer@icloud.com>
2022-11-14 21:19:28 +0900

    fix

---
+++ fetch-sample/src/types/Task.ts
@@ -0,0 +1,7 @@
+export type Task = {
+  id: number;
+  name: string;
+  status: string;
+};
+
+export type TaskList = Task[];
--- fetch-sample/src/utils/$path.ts
+++ fetch-sample/src/utils/$path.ts
@@ -2,6 +2,9 @@ export const pagesPath = {
   "add_post": {
     $url: (url?: { hash?: string }) => ({ pathname: '/add-post' as const, hash: url?.hash })
   },
+  "tasks": {
+    $url: (url?: { hash?: string }) => ({ pathname: '/tasks' as const, hash: url?.hash })
+  },
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
